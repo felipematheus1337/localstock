5e12cc507eb8958edb139ba5e63e3fdf879010d7
felipematheus1337 <lipehbr@gmail.com>
2025-04-08 12:58:09 -0300

    criado appContext

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Localstockk.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Localstockk.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        private readonly string DB_URL = "";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DB_URL);
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+
+
+    }
+}
